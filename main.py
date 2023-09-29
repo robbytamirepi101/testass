80ab36686d87202e3726a45723d0b2b90f62798c
robbytamirepi101 <146491597+robbytamirepi101@users.noreply.github.com>
2023-09-29 23:51:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#You can enter investment rate as a whole number like 5
+interestRate = int(input("Please enter your yearly rate: " ))
+print()
+
+#You can enter the inverstment amount as a full amount like 50000
+investmentAmount = float(input("Please enter your investment amount: "))
+
+newinvestmentAmount = investmentAmount
+
+year = 0
+
+print()    
+while newinvestmentAmount <= (investmentAmount * 2):
+    newinvestmentAmount = newinvestmentAmount + (newinvestmentAmount * (interestRate / 100))
+    year +=1
+    print("The investment for the year : " + str (year)+ " is "  + str(round(newinvestmentAmount, 2)))
+    
+print()
+print("It will take ", str (year) + " years to double your investment" )
