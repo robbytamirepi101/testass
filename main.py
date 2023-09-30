7f69a700f08456d70f823c80bdec614d253982fd
rtamirepi <25348387-rtamirepi@users.noreply.replit.com>
2023-09-30 00:09:36 +0000

    spellcheck

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 interestRate = int(input("Please enter your yearly rate: " ))
 print()
 
-#You can enter the inverstment amount as a full amount like 50000
+#You can enter the investment amount as a full amount like 50000
 investment_amount = float(input("Please enter your investment amount: "))
 
 newinvestmentAmount = investment_amount = float(input("Please enter your investment amount: "))
@@ -16,7 +16,7 @@ while newinvestmentAmount <= (investmentAmount * 2):
     print("The investment for the year : " + str (year)+ " is "  + str(round(newinvestmentAmount, 2)))
 
 
-print("we checking for chnages on code")
+print("we checking for changes on code")
 print()
 print("It will take ", str (year) + " years to double your investment" )
 #added a comment that we changed the code and testing 
